0200998cdb12d5c6771fe324a48c80e0ff2f2e71
ssangttuce <sanghoo1023@gmail.com>
2024-08-07 15:16:51 +0900

    models update

--- convetingBE/models.py
+++ convetingBE/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Pets(models.Model):
     owner = models.CharField(max_length=30, primary_key=True)
-    name = models.CharField(max_length=15, primary_key=True)
+    name = models.CharField(max_length=20, primary_key=True)
     
 class Disease(models.Model):
     disease = models.CharField(max_length=255, primary_key=True)
@@ -10,14 +10,30 @@ class Disease(models.Model):
 
 class ResultAI(models.Model):
     seq = models.PositiveIntegerField(primary_key=True)
-    predicted_disease1 = models.DecimalField(max_digits=5, decimal_places=2)
-    predicted_disease2 = models.DecimalField(max_digits=5, decimal_places=2)
-    predicted_disease3 = models.DecimalField(max_digits=5, decimal_places=2)
+    skin1 = models.DecimalField(max_digits=5, decimal_places=2)
+    skin2 = models.DecimalField(max_digits=5, decimal_places=2)
+    skin3 = models.DecimalField(max_digits=5, decimal_places=2)
+    skin4 = models.DecimalField(max_digits=5, decimal_places=2)
+    skin5 = models.DecimalField(max_digits=5, decimal_places=2)
+    skin6 = models.DecimalField(max_digits=5, decimal_places=2)
+    skin7 = models.DecimalField(max_digits=5, decimal_places=2)
+    eye1 = models.DecimalField(max_digits=5, decimal_places=2)
+    eye2 = models.DecimalField(max_digits=5, decimal_places=2)
+    eye3 = models.DecimalField(max_digits=5, decimal_places=2)
+    eye4 = models.DecimalField(max_digits=5, decimal_places=2)
+    eye5 = models.DecimalField(max_digits=5, decimal_places=2)
+    eye6 = models.DecimalField(max_digits=5, decimal_places=2)
+    eye7 = models.DecimalField(max_digits=5, decimal_places=2)
+    eye8 = models.DecimalField(max_digits=5, decimal_places=2)
+    eye9 = models.DecimalField(max_digits=5, decimal_places=2)
+    eye10 = models.DecimalField(max_digits=5, decimal_places=2)
+    eye11 = models.DecimalField(max_digits=5, decimal_places=2)
+    
 
 class Diagnosis(models.Model):
-    owner = models.ForeignKey(Pets, on_delete=models.CASCADE)
-    pet = models.ForeignKey(Pets, on_delete=models.CASCADE)
-    seq = models.ForeignKey(ResultAI, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Pets, on_delete=models.CASCADE, primary_key=True)
+    pet = models.ForeignKey(Pets, on_delete=models.CASCADE, primary_key=True)
+    seq = models.ForeignKey(ResultAI, on_delete=models.CASCADE, primary_key=True)
     datetime = models.DateTimeField(auto_now_add=True)
     photo = models.CharField(max_length=255)
 
